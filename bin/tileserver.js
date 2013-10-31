50521d97708361aec8d5ab9fc55e306236ff0238
Seth Fitzsimmons <seth@mojodna.net>
2013-10-30 17:36:02 -0700

    Load metatile, buffer size, and scale from the stylesheet

--- bin/tileserver.js
+++ bin/tileserver.js
@@ -12,7 +12,8 @@ var async = require("async"),
     metricsd = require("metricsd"),
     SphericalMercator = require("sphericalmercator"),
     tilelive = require("tilelive"),
-    tileliveMapnik = require("tilelive-mapnik");
+    tileliveMapnik = require("tilelive-mapnik"),
+    xml2js = require("xml2js");
 
 tileliveMapnik.registerProtocols(tilelive);
 
@@ -21,11 +22,6 @@ var metrics = metricsd({
 });
 
 var merc = new SphericalMercator();
-
-var SCALE = process.env.SCALE || 1;
-var METATILE = process.env.METATILE || 4;
-var BUFFER_SIZE = process.env.BUFFER_SIZE || 128;
-var TILE_SIZE = process.env.TILE_SIZE || 256;
 
 // TODO extract app into lib/app.js
 var app = express();
@@ -106,126 +102,161 @@ stylesheet = stylesheet
 var stylesheetPath = path.join(process.cwd(), "style.xml");
 fs.writeFileSync(stylesheetPath, stylesheet);
 
-async.series([
+async.waterfall([
   function(done) {
-    tilelive.load({
-      protocol: "mapnik:",
-      hostname: "",
-      pathname: stylesheetPath,
-      query: {
-        metatile: METATILE, // TODO get this out of stylesheet.xml (info)
-        bufferSize: BUFFER_SIZE, // TODO get this out of stylesheet.xml (info)
-        tileSize: TILE_SIZE, // TODO get this out of stylesheet.xml (info)
-        scale: SCALE // TODO get this out of stylesheet.xml (info)
-      }
-    }, function(err, source) {
+    return xml2js.parseString(stylesheet, function(err, doc) {
       if (err) {
-        console.error(err);
-        process.exit(1);
+        return done(err);
       }
 
-      console.log("@1x initialized.");
+      // set some defaults
+      var params = {
+        scale: 1,
+        metatile: 1,
+        bufferSize: doc.Map.$["buffer-size"] || 0,
+        tileSize: 256
+      };
 
-      var info = source._info || {};
-      info.minzoom = info.minzoom || 0;
-      info.maxzoom = info.maxzoom || Infinity;
-      info.bounds = info.bounds || [-180, -90, 180, 90];
-      info.scale = info.scale || SCALE;
-
-      var format = info.format || "png";
-
-      app.get(new RegExp("^/(\\d+)/(\\d+)/(\\d+)\\." + format),
-              makeRenderHandler(info, source));
-
-      app.get(new RegExp("^/(\\d+)/(\\d+)/(\\d+)\\.json"), function(req, res) {
-        var z = +req.params[0],
-            x = +req.params[1],
-            y = +req.params[2];
-
-        // validate zoom
-        if (z < info.minzoom || z > info.maxzoom) {
-          return res.send(404);
-        }
-
-        // validate coords against bounds
-        var xyz = merc.xyz(info.bounds, z);
-
-        if (x < xyz.minX ||
-            x > xyz.maxX ||
-            y < xyz.minY ||
-            y > xyz.maxY) {
-          return res.send(404);
-        }
-
-        source.getGrid(z, x, y,
-                       metrics.timeCallback("render.grid." + info.scale + "x.z" + z, function(err, tile, headers) {
-          if (err) {
-            console.warn(err);
-            return res.send(500);
-          }
-
-          res.set(headers);
-
-          // TODO configurable max-age
-          res.set("Cache-Control", "public,max-age=300");
-
-          res.send(tile);
-        }));
+      doc.Map.Parameters[0].Parameter.forEach(function(param) {
+        params[param.$.name] = param._;
       });
 
-      setInterval(function() {
-        Object.keys(source._stats).forEach(function(k) {
-          metrics.updateGauge("%sx.%s", info.scale, k, source._stats[k]);
-        });
-      }, 30000);
+      // environmental overrides
+      params.scale = process.env.SCALE || params.scale;
+      params.metatile = process.env.METATILE || params.metatile;
+      params.bufferSize = process.env.BUFFER_SIZE || params.bufferSize;
+      params.tileSize = process.env.TILE_SIZE || params.tileSize;
 
-      return done();
+      return done(null, params);
     });
   },
-  function(done) {
-    tilelive.load({
-      protocol: "mapnik:",
-      hostname: "",
-      pathname: stylesheetPath,
-      query: {
-        metatile: METATILE,
-        bufferSize: BUFFER_SIZE * 2,
-        tileSize: TILE_SIZE * 2,
-        scale: SCALE * 2
-      }
-    }, function(err, source) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
+  function(params, done) {
+    return async.parallel([
+      function(done) {
+        tilelive.load({
+          protocol: "mapnik:",
+          hostname: "",
+          pathname: stylesheetPath,
+          query: {
+            metatile: params.metatile,
+            bufferSize: params.bufferSize,
+            tileSize: params.tileSize,
+            scale: params.scale
+          }
+        }, function(err, source) {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
 
-      console.log("@2x initialized.");
+          console.log("@1x initialized.");
 
-      var info = source._info || {};
-      info.minzoom = info.minzoom || 0;
-      info.maxzoom = info.maxzoom || Infinity;
-      info.bounds = info.bounds || [-180, -90, 180, 90];
-      info.scale = info.scale || SCALE * 2;
+          var info = source._info || {};
+          info.minzoom = info.minzoom || 0;
+          info.maxzoom = info.maxzoom || Infinity;
+          info.bounds = info.bounds || [-180, -90, 180, 90];
+          info.scale = info.scale || SCALE;
 
-      var format = info.format || "png";
+          var format = info.format || "png";
 
-      app.get(new RegExp("^/(\\d+)/(\\d+)/(\\d+)@2x\\." + format),
-              makeRenderHandler(info, source));
+          app.get(new RegExp("^/(\\d+)/(\\d+)/(\\d+)\\." + format),
+                  makeRenderHandler(info, source));
 
-      setInterval(function() {
-        Object.keys(source._stats).forEach(function(k) {
-          metrics.updateGauge("%dx.%s", info.scale, k, source._stats[k]);
+          app.get(new RegExp("^/(\\d+)/(\\d+)/(\\d+)\\.json"), function(req, res) {
+            var z = +req.params[0],
+                x = +req.params[1],
+                y = +req.params[2];
+
+            // validate zoom
+            if (z < info.minzoom || z > info.maxzoom) {
+              return res.send(404);
+            }
+
+            // validate coords against bounds
+            var xyz = merc.xyz(info.bounds, z);
+
+            if (x < xyz.minX ||
+                x > xyz.maxX ||
+                y < xyz.minY ||
+                y > xyz.maxY) {
+              return res.send(404);
+            }
+
+            source.getGrid(z, x, y,
+                          metrics.timeCallback("render.grid." + info.scale + "x.z" + z, function(err, tile, headers) {
+              if (err) {
+                console.warn(err);
+                return res.send(500);
+              }
+
+              res.set(headers);
+
+              // TODO configurable max-age
+              res.set("Cache-Control", "public,max-age=300");
+
+              res.send(tile);
+            }));
+          });
+
+          setInterval(function() {
+            Object.keys(source._stats).forEach(function(k) {
+              metrics.updateGauge("%sx.%s", info.scale, k, source._stats[k]);
+            });
+          }, 30000);
+
+          return done();
         });
-      }, 30000);
+      },
+      function(done) {
+        tilelive.load({
+          protocol: "mapnik:",
+          hostname: "",
+          pathname: stylesheetPath,
+          query: {
+            metatile: params.metatile,
+            bufferSize: params.bufferSize * 2,
+            tileSize: params.tileSize * 2,
+            scale: params.scale * 2
+          }
+        }, function(err, source) {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
 
-      return done();
-    });
+          console.log("@2x initialized.");
+
+          var info = source._info || {};
+          info.minzoom = info.minzoom || 0;
+          info.maxzoom = info.maxzoom || Infinity;
+          info.bounds = info.bounds || [-180, -90, 180, 90];
+          info.scale = info.scale || SCALE * 2;
+
+          var format = info.format || "png";
+
+          app.get(new RegExp("^/(\\d+)/(\\d+)/(\\d+)@2x\\." + format),
+                  makeRenderHandler(info, source));
+
+          setInterval(function() {
+            Object.keys(source._stats).forEach(function(k) {
+              metrics.updateGauge("%dx.%s", info.scale, k, source._stats[k]);
+            });
+          }, 30000);
+
+          return done();
+        });
+      }
+    ], done);
+  },
+], function(err) {
+  if (err) {
+    console.error(err.stack);
+    process.exit(1);
   }
-], function() {
+
   console.log("Mapnik initialized.");
-});
 
-app.listen(process.env.PORT || 8080, function() {
-  console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+  app.listen(process.env.PORT || 8080, function() {
+    console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+  });
 });
-
